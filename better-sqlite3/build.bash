1526e970323181b5ddeed8bfd913bd8720cdd7a4
uttne <uttne.mkr@gmail.com>
2023-05-02 22:17:23 +0900

    コメント追加

--- better-sqlite3/build.bash
+++ better-sqlite3/build.bash
@@ -1,10 +1,24 @@
 #!/bin/bash
+
+# 作業ディレクトリに移動
 cd /work
+
+# ビルドに必要なパッケージをインストール
 yum install git python3 make gcc gcc-c++ -y
+
+# ビルド結果があったときはクリアしておく
 if [ -d ./better-sqlite3 ]; then
     rm -fr ./better-sqlite3
 fi
-git clone https://github.com/WiseLibs/better-sqlite3.git --depth=1
+
+# better-sqlite3 のソースをダウンロードする
+git clone https://github.com/WiseLibs/better-sqlite3.git -b v8.3.0 --depth=1
+
+# ソースのフォルダに移動する
 cd better-sqlite3
+
+# ビルドコマンドを実行する
 npm install
+
+# テストを実行して動くことを確認する
 npm test
